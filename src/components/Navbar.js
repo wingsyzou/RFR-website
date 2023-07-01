05ce37f0af9db2f219aee04063bdda85498b3d8c
wingsyzou <wing.zou@mail.utoronto.ca>
2023-07-01 16:11:40 -0400

    implemented navbar hiddenlinks function for various screen sizes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import Logo from "../assets/wood.png";
 import {Link} from "react-router-dom";
 import "../css/Navbar.css";
 import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Navbar() {
+  const [openLinks, setOpenLinks] = useState(false)
+  
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks) 
+  };
+
   return (
     <div className="navbar">
-      <div className="leftSide">
+      <div className="leftSide" id={openLinks ? "open" : "close"}>
         <img src={Logo} />
-        <Link to="/"> Rapid Fence Renewals </Link>
+        <h1> Rapid Fence Renewals </h1>
+        <div className="hiddenLinks">
+          <Link to="/"> Home </Link>
+          <Link to="/packages"> Packages </Link>
+          <Link to="/about"> About </Link>
+          <Link to="/contact"> Contact </Link>
+        </div>
       </div>
       <div className="rightSide">
         <Link to="/"> Home </Link>
         <Link to="/packages"> Packages </Link>
         <Link to="/about"> About </Link>
         <Link to="/contact"> Contact </Link>
-        <button><ReorderIcon /></button>
+        <button onClick={toggleNavbar}>
+          <ReorderIcon />
+        </button>
       </div>
     </div>
   )

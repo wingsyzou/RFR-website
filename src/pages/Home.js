8bf221de222bf900ab5abe2d040992a77ad745fe
wingsyzou <wing.zou@mail.utoronto.ca>
2023-07-01 23:00:02 -0400

    added home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Banner from "../assets/backyard.jpg";
+import "../css/Home.css";
 
 function Home() {
-  return <div></div>;
+  return (
+    <div className="home" style={{ background: `url(${Banner})` }}>
+      <div className="headerContainer">
+        <h1> Your Partner in Fence Restoration </h1>
+
+        {/* add book function. ie asking for quote */}
+        <button> BOOK NOW </button>  
+
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;

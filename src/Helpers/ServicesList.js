c21de5c2699011042cecec0399d8944557ebe1a5
Wing Zou <you@example.com>
2023-07-02 22:57:14 -0400

    changed home page style and added service helper

---
+++ src/Helpers/ServicesList.js
@@ -0,0 +1,18 @@
+export const ServicesList = [
+    {
+        name: "Pressure Washing",
+        image: PressureWasher,
+    },
+    {
+        name: "Sanding",
+        image: Sander,
+    },
+    {
+        name: "Painting",
+        image: PaintJob,
+    },
+    {
+        name: "Cleaning",
+        image: Cleaning,
+    }
+]
---
+++ src/pages/Services.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Services() {
+  return (
+    <div className="services">
+      <h1 className="title">Our Services</h1>
+      <div className="servicesList">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Services

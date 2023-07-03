5ce43d7ec7faf4eddc174ce2eb039f31afda7095
wingsyzou <wing.zou@mail.utoronto.ca>
2023-07-03 19:13:38 -0400

    added input form and other links under home banner section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,41 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
 import "../css/Home.css";
 
 function Home() {
+  const [postalCode, setPostalCode] = useState("")
+  const navigate = useNavigate()
+
+  const handleChange = (event) => {
+    setPostalCode(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate('/book-now')
+  };
+
   return (
     <div className="home">
       <div className="header">
         <h1> Your Partner in Fence Restoration </h1>
       </div>
       <div className="consultation">
-        <div className="consultation-content">
-          <p> Book a free consultation to inquire about our services </p>  
-        </div>
-        <div className="consultation-button">
-          {/* Add book function. E.g., asking for a quote */}
-          <button> BOOK NOW </button>
-        </div>
+        <Link to="/services"> All Services </Link>
+        <Link to="/services/packages"> Packages </Link>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={postalCode}
+            onChange={handleChange}
+            placeholder="Postal Code"
+          />
+          <button type="submit"> Book A Consultation </button>
+        </form>
+      </div>
+      <div className="reviews">
+
       </div>
     </div>
   );

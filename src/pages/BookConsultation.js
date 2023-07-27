1a358f0ba803c6a01ebaa311b1be3c4050fc21fd
wingsyzou <wing.zou@mail.utoronto.ca>
2023-07-27 19:45:18 -0400

    made the book consultation page look prettier

--- src/pages/BookConsultation.js
+++ src/pages/BookConsultation.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import '../css/BookConsultation.css';
+import consultationImage from "../assets/white-fence.png";
 
 function BookConsultation() {
   const [name, setName] = useState("");
@@ -53,52 +54,57 @@ function BookConsultation() {
   };
 
   return (
-    <div className="service-details">
-      <h1>Book Consultation</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name (required)"
-          value={name}
-          onChange={handleInputChange}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email (required)"
-          value={email}
-          onChange={handleInputChange}
-        />
-        <input
-          type="tel"
-          name="phone"
-          placeholder="Phone"
-          value={phone}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="address1"
-          placeholder="Address Line 1 (required)"
-          value={address1}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="address2"
-          placeholder="Address Line 2"
-          value={address2}
-          onChange={handleInputChange}
-        />
-        <textarea
-          name="serviceDetails"
-          placeholder="Service Details (required)"
-          value={serviceDetails}
-          onChange={handleInputChange}
-        ></textarea>
-        <button type="submit">Submit</button>
-      </form>
+    <div className="book-consultation">
+      <div className="image-container">
+        <img src={consultationImage} alt="Consultation" />
+      </div>
+      <div className="service-details">
+        <h1>Book Consultation</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name (required)"
+            value={name}
+            onChange={handleInputChange}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email (required)"
+            value={email}
+            onChange={handleInputChange}
+          />
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Phone"
+            value={phone}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="address1"
+            placeholder="Address Line 1 (required)"
+            value={address1}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="address2"
+            placeholder="Address Line 2"
+            value={address2}
+            onChange={handleInputChange}
+          />
+          <textarea
+            name="serviceDetails"
+            placeholder="Service Details (required)"
+            value={serviceDetails}
+            onChange={handleInputChange}
+          ></textarea>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }

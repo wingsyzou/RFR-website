5ebe8b823f75fb52082ee1b08921efdeba6ea48f
Wing Zou <you@example.com>
2023-09-04 15:31:03 -0400

    backend works but console logging is not working

--- backend/index.js
+++ backend/index.js
@@ -2,39 +2,69 @@ require('dotenv').config();
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+const createTransporter = async () => {
+  try {
+    const oauth2Client = new OAuth2(
+        process.env.OAUTH_CLIENT_ID,
+        process.env.OAUTH_CLIENT_SECRET,
+        "https://developers.google.com/oauthplayground"
+      );
+
+      oauth2Client.setCredentials({
+        refresh_token: process.env.OAUTH_REFRESH_TOKEN,
+      });
+
+      const accessToken = await new Promise((resolve, reject) => {
+        oauth2Client.getAccessToken((err, token) => {
+          if (err) {
+            console.log("*ERR: ", err)
+            reject();
+          }
+          resolve(token); 
+        });
+      });
+
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          type: "OAuth2",
+          user: process.env.EMAIL_USER,
+          accessToken,
+          clientId: process.env.OAUTH_CLIENT_ID,
+          clientSecret: process.env.OAUTH_CLIENT_SECRET,
+          refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+        },
+      });
+      return transporter;
+  } catch (err) {
+    return err
+  }
+};
+
 app.post('/send-email', async (req, res) => {
   const { name, email, phone, address1, address2, serviceDetails, postalCode } = req.body;
-
-  // Validation
+  
   if (!name || !email || !address1 || !serviceDetails) {
     return res.status(400).send('All fields are required');
   }
 
-  // Initialize nodemailer transporter
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-
-  // Set up email data
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to: process.env.EMAIL_USER,
     subject: `New Booking From ${name}`,
-    text: `${name} wants to book a consultation.\nEmail: ${email}\nPhone: ${phone}\nAddress1: ${address1}\nAddress2: ${address2}\nPostal Code: ${postalCode}\nService Detals: ${serviceDetails}`,
+    text: `${name} wants to book a consultation.\nEmail: ${email}\nPhone: ${phone}\nAddress1: ${address1}\nAddress2: ${address2}\nPostal Code: ${postalCode}\nService Details: ${serviceDetails}`,
   };
 
-  // Send email
   try {
+    const transporter = await createTransporter();
     await transporter.sendMail(mailOptions);
     res.status(200).send('Email sent successfully');
   } catch (error) {
